4e668207fa556d02b8bfb7cbc54eb9400fc4a7d7
Brad <bradsiefert@icloud.com>
2020-08-18 16:19:08 -0500

    Adding in font awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret, faClipboard } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret, faClipboard)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 import './assets/scss/app.scss'
 
 Vue.config.productionTip = false

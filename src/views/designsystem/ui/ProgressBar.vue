cd2229826e89b5919d5d904b2ecc77853d64a221
Brad <bradsiefert@icloud.com>
2020-08-25 15:03:08 -0500

    Creating the Progress bar

---
+++ src/views/designsystem/ui/ProgressBar.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container skinny-contain">
+    <div class="row">
+      <div class="col-sm">
+        <breadcrumbs :previous="{ '/': 'Home', '/designsystem': 'Design System' }" active="Progress Bar"/>
+
+        <h1>Progress Bar</h1>
+        <p>
+          Coming Soon
+        </p>
+        
+        <!-- Start code chunk -->
+        <div class="card card-example">
+          <div class="card-body">
+            <h3 class="card-example-heading">Progress Bar</h3>
+            <div class="progress mb-3">
+              <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="progress mb-3">
+              <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="progress mb-3">
+              <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="progress mb-3">
+              <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="progress mb-3">
+              <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            
+            <hr>
+
+            <h3 class="card-example-heading">Height, Color & Label</h3>
+            <p>
+              The default height of the progress bar is 0.5rem or 8px. You can update the height of the bar by adding 
+              <code>style="height: 4px;"</code> to the <code>progress</code> surrounding div. To add a label, add your 
+              copy inside the <code>progress-bar</code> div. The color of the bar can be updated by using the standard 
+              template colors like <code>bg-info</code>.
+            </p> 
+
+            <div class="progress mb-3" style="height: 1px;">
+              <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="progress mb-3" style="height: 4px;">
+              <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="progress mb-3" style="height: 16px;">
+              <div class="progress-bar bg-info" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+            </div>
+
+          </div>
+        </div>
+        <div class="card card-body code-example">
+          <div class="d-flex flex-row align-items-center justify-content-between">
+            <h6 class="code-example-heading">// HTML</h6>
+          </div>
+
+          <div v-highlight>
+            <pre class="language-html"><code></code></pre>
+          </div>
+        </div> 
+        <!-- End code chunk -->
+
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-sm">
+        <div class="card my-4">
+          <div class="card-body card-body-sm text-center">
+            For more info → <a target="_blank" href="https://v5.getbootstrap.com/docs/5.0/components/progress/">
+              Bootstrap v5 / Progress Bar
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div> <!-- div.container -->
+</template>
+
+<script>
+import Breadcrumbs from '@/components/ui/Breadcrumbs'
+
+export default {
+  components: {
+    Breadcrumbs
+  }
+}
+</script>

677e20c508b4ae018985f5453b358ea93cf5df87
Brad Siefert <bradsiefert@icloud.com>
2020-12-08 16:33:13 -0600

    Adding Github component

---
+++ src/components/GithubLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card my-4">
+    <div class="card-body card-body-sm text-center">
+      Full Release History →
+      <a target="_blank" href="https://github.com/bradsiefert/highbeam-design-system">
+        Highbeam Design System on Github
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GithubLink'
+}
+</script>
